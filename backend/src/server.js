e4006b21592637d0cf00875991869bee8a9a1ef0
minnu04 <tadikondasrivardhini9@gmail.com>
2025-12-25 22:36:11 +0530

    Express setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import app from './app.js';
-import connectDB  from './config/db';
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 2204;
-
 connectDB();
 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
