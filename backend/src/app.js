6af965569195dd176efec3048d1f5704dab4f544
minnu04 <tadikondasrivardhini9@gmail.com>
2025-12-28 22:47:52 +0530

    day 4 done

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
+import campaignRoutes from './routes/campaignRoutes.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', profileRoutes);
+app.use('/api/campaigns', campaignRoutes);
 
 app.get('/', (req, res) => {
     res.send(' 🐾 FurEverHelp API is running');
