f9a8ba6b84877b0f7942ff8be955de408832fe11
minnu04 <tadikondasrivardhini9@gmail.com>
2026-01-21 15:23:17 +0530

    CRUD Campaign done

--- backend/src/controllers/campaignController.js
+++ backend/src/controllers/campaignController.js
@@ -1,7 +1,5 @@
 import Campaign from '../models/Campaign.js';
 
-// CREATE CAMPAIGN
-// POST /api/campaigns (Owner/Shelter)
 export const createCampaign = async (req, res) => {
   try {
     const campaign = await Campaign.create({
@@ -19,6 +17,16 @@ export const createCampaign = async (req, res) => {
   }
 };
 
+export const getMyCampaigns = async (req,res)=>{
+  try{
+    const campaigns = await Campaign.find({
+      createdBy: req.user._id})
+    .sort({ createdAt: -1 });
+    res.status(200).json(campaigns);
+  }catch(error){
+    res.status(500).json({ message: error.message });
+  }
+}
 // GET ALL CAMPAIGNS
 export const getCampaigns = async (req, res) => {
   try {
@@ -50,3 +58,50 @@ export const getCampaignById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateCampaign = async (req, res) => {
+  try {
+    const campaign = await Campaign.findById(req.params.id);
+
+    if (!campaign) {
+      return res.status(404).json({ message: 'Campaign not found' });
+    }
+
+    if (campaign.createdBy.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Not authorized to update this campaign' });
+    } 
+
+    campaign.title = req.body.title || campaign.title;
+    campaign.description = req.body.description || campaign.description;
+    campaign.category = req.body.category || campaign.category;
+    campaign.pet = req.body.pet || campaign.pet;
+    campaign.goalAmount = req.body.goalAmount || campaign.goalAmount;
+    campaign.deadline = req.body.deadline || campaign.deadline;
+
+    campaign.campaignStatus = 'Pending';
+
+    const updatedCampaign = await campaign.save();
+    res.status(200).json(updatedCampaign);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteCampaign = async (req, res) => {
+  try {
+    const campaign = await Campaign.findById(req.params.id);
+    if (!campaign) {
+      return res.status(404).json({ message: 'Campaign not found' });
+    }
+    if (campaign.createdBy.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Not authorized to delete this campaign' });
+    }
+    await campaign.deleteOne();
+
+    res.status(200).json({ message: 'Campaign deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
